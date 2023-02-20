cf269a43d4512939ddd2361c1e7866aff436add1
collblock <andidol500@gmail.com>
2023-02-21 01:04:37 +0300

    del

--- bot/reset.py
+++
@@ -1,6 +0,0 @@
-from openpyxl import load_workbook
-
-with open('bot\count_run.py', 'w') as file:  # накидываем на счетчик +1
-        file.write(f'n = 1')
-wb = load_workbook(filename='origin.xlsx')
-wb.save('way_to_dream.xlsx')
