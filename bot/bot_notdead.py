bb0ff74d27d05db8d1e43f369bbdd7b6706aaa68
collblock <andidol500@gmail.com>
2023-02-21 16:18:32 +0300

    COOL

--- bot/bot_notdead.py
+++ bot/bot_notdead.py
@@ -1,10 +1,11 @@
-import telebot
 import threading
-import work_data
-
+from pathlib import Path
 from random import randint
-from config import token
 from time import sleep
+
+import telebot
+import work_data
+from config import token
 
 BOT_TOKEN = token
 BOT_INTERVAL = 3
@@ -12,74 +13,95 @@ BOT_TIMEOUT = 30
 
 bot = None
 types = telebot.types
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 def bot_polling():
-    global bot
+    global bot, BASE_DIR
     print("Starting bot polling now")
     while True:
         try:
             print("New bot instance started")
-            bot = telebot.TeleBot(BOT_TOKEN) #Generate new bot instance
-            botactions() #If bot is used as a global variable, remove bot as an input param
-            bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
-        except Exception as ex: #Error in polling
-            print("Bot polling failed, restarting in {}sec. Error:\n{}".format(BOT_TIMEOUT, ex))
+            bot = telebot.TeleBot(BOT_TOKEN)  # Generate new bot instance
+            botactions()  # If bot is used as a global variable, remove bot as an input param
+            bot.polling(none_stop=True, interval=BOT_INTERVAL,
+                        timeout=BOT_TIMEOUT)
+        except Exception as ex:  # Error in polling
+            print("Bot polling failed, restarting in {}sec. Error:\n{}".format(
+                BOT_TIMEOUT, ex))
             bot.stop_polling()
             sleep(BOT_TIMEOUT)
-        else: #Clean exit
+        else:  # Clean exit
             bot.stop_polling()
             print("Bot polling loop finished")
-            break #End loop
+            break  # End loop
 
 
 def botactions():
     global types
-    #отработка /start
+
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAIfF2PszletHz55Wfxy6K4JPqU-O-rEAAILAAMOR8coqKD0-uKs4cEuBA')
-        keyboard=types.InlineKeyboardMarkup()
-        start_work=types.InlineKeyboardButton(text='Работой 💼', callback_data='start_work')
-        statistic=types.InlineKeyboardButton(text='Анализом 📊', callback_data='static')
+        bot.send_sticker(
+            message.chat.id, 'CAACAgIAAxkBAAIfF2PszletHz55Wfxy6K4JPqU-O-rEAAILAAMOR8coqKD0-uKs4cEuBA')
+        keyboard = types.InlineKeyboardMarkup()
+        start_work = types.InlineKeyboardButton(
+            text='Работой 💼', callback_data='start_work')
+        statistic = types.InlineKeyboardButton(
+            text='Анализом 📊', callback_data='static')
         keyboard.add(start_work, statistic)
 
-        bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n - <b>{1.first_name}</b>, бот созданный чтобы помогать мне ввести учет доходов.".format(message.from_user, bot.get_me()), parse_mode='html')
-        bot.send_message(message.chat.id,'Выбери, чем мы сейчас займемся',reply_markup=keyboard)
+        bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\n - <b>{1.first_name}</b>, бот созданный чтобы помогать мне ввести учет доходов.".format(
+            message.from_user, bot.get_me()), parse_mode='html')
+        bot.send_message(
+            message.chat.id, 'Выбери, чем мы сейчас займемся', reply_markup=keyboard)
 
     @bot.message_handler(commands=['replace_tabl'])
     def help_message(message):
-        data = json.load(open('bot\count_run.json'))
+        data = json.load(open(f'{BASE_DIR}/bot/count_run.json'))
         n = data["count_run"]
-        bot.send_message(message.chat.id, f"<b>Для смены таблицы отправьте ее файл</b>\n<i>Для работы нужен лист с названием новая сводка</i>\n<b>Количество запусов</b>: <code>{n}</code>",parse_mode='html')
+        bot.send_message(
+            message.chat.id, f"<b>Для смены таблицы отправьте ее файл</b>\n<i>Для работы нужен лист с названием новая сводка</i>\n<b>Количество запусов</b>: <code>{n}</code>\nСмена количества n = *ваше число*", parse_mode='html')
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_inline(call):
         if call.message:
-            if call.data == 'start_work': 
-                #реакция при нажатии inline кнопки начать работать
-                keyboard=types.InlineKeyboardMarkup()
-                dosty=types.InlineKeyboardButton(text='🕊', callback_data='dosty')
-                video=types.InlineKeyboardButton(text='🎞', callback_data='video')
-                photo=types.InlineKeyboardButton(text='📸', callback_data='photo')
-                other=types.InlineKeyboardButton(text='...', callback_data='other')
-                back=types.InlineKeyboardButton(text='◀️', callback_data='back')
+            if call.data == 'start_work':
+                # реакция при нажатии inline кнопки начать работать
+                keyboard = types.InlineKeyboardMarkup()
+                dosty = types.InlineKeyboardButton(
+                    text='🕊', callback_data='dosty')
+                video = types.InlineKeyboardButton(
+                    text='🎞', callback_data='video')
+                photo = types.InlineKeyboardButton(
+                    text='📸', callback_data='photo')
+                other = types.InlineKeyboardButton(
+                    text='...', callback_data='other')
+                back = types.InlineKeyboardButton(
+                    text='◀️', callback_data='back')
                 keyboard.add(dosty, video, photo, other, back)
-                
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='<b>ООО! Круто, дружище!\n</b>Хмм, а что именно будешь делать?', reply_markup=keyboard,parse_mode='html')
-                bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text='Начало работы')
-            
-            #отработка всех кнопок с типами работ с внесение в таблицу в зависимости от кнопки
-            elif call.data == 'dosty' or call.data == 'video' or call.data == 'photo' or call.data == 'other': 
-                big_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text='<b>ООО! Круто, дружище!\n</b>Хмм, а что именно будешь делать?', reply_markup=keyboard, parse_mode='html')
+                bot.answer_callback_query(
+                    callback_query_id=call.id, show_alert=False, text='Начало работы')
+
+            # отработка всех кнопок с типами работ с внесение в таблицу в зависимости от кнопки
+            elif call.data == 'dosty' or call.data == 'video' or call.data == 'photo' or call.data == 'other':
+                big_keyboard = types.ReplyKeyboardMarkup(
+                    resize_keyboard=True, one_time_keyboard=True)
                 end = types.KeyboardButton('Закончить работу')
                 big_keyboard.add(end)
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='<b>Такс!</b>\nЯ записал, когда ты начал работать.\n<b><i>Удачной работы, брат!</i></b>',parse_mode='html',reply_markup=None)
-                bot.send_message(call.message.chat.id, '_', reply_markup=big_keyboard)
-                bot.answer_callback_query(callback_query_id=call.id, show_alert=False, text='Ах, жду когда закончится твоя смена')
-                work_data.dates() 
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text='<b>Такс!</b>\nЯ записал, когда ты начал работать.\n<b><i>Удачной работы, брат!</i></b>', parse_mode='html', reply_markup=None)
+                bot.send_message(call.message.chat.id, '_',
+                                 reply_markup=big_keyboard)
+                bot.answer_callback_query(
+                    callback_query_id=call.id, show_alert=False, text='Ах, жду когда закончится твоя смена')
+                work_data.dates()
                 work_data.start()
-                
-                if call.data == 'dosty':    
+
+                if call.data == 'dosty':
                     work_data.type('Доставка')
                 elif call.data == 'video':
                     work_data.type('Монтаж')
@@ -87,19 +109,23 @@ def botactions():
                     work_data.type('Фото ')
                 elif call.data == 'other':
                     work_data.type('Другое')
-        
+
             elif call.data == 'back':
-                keyboard=types.InlineKeyboardMarkup()
-                start_work=types.InlineKeyboardButton(text='Работой 💼', callback_data='start_work')
-                statistic=types.InlineKeyboardButton(text='Анализом 📊', callback_data='static')
+                keyboard = types.InlineKeyboardMarkup()
+                start_work = types.InlineKeyboardButton(
+                    text='Работой 💼', callback_data='start_work')
+                statistic = types.InlineKeyboardButton(
+                    text='Анализом 📊', callback_data='static')
                 keyboard.add(start_work, statistic)
 
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='Выбери, чем мы сейчас займемся',reply_markup=keyboard)
-            
-            #отработка всех кнопок с кол-вом заказов
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text='Выбери, чем мы сейчас займемся', reply_markup=keyboard)
+
+            # отработка всех кнопок с кол-вом заказов
             elif call.data == 'one' or call.data == 'two' or call.data == 'three' or call.data == 'four' or call.data == 'five':
-                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,text='<b>Записано</b>\nСколько заработал?',parse_mode='html')
-                if call.data =='one':
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                      text='<b>Записано</b>\nСколько заработал?', parse_mode='html')
+                if call.data == 'one':
                     work_data.counts(1)
                 elif call.data == 'two':
                     work_data.counts(2)
@@ -111,8 +137,8 @@ def botactions():
                     work_data.counts(5)
 
             elif call.data == 'static':
-                excel = open('way_to_dream.xlsx','rb')
-                bot.send_document(call.message.chat.id,excel)
+                excel = open(f'{BASE_DIR}/way_to_dream.xlsx', 'rb')
+                bot.send_document(call.message.chat.id, excel)
 
     @bot.message_handler(content_types=['document'])
     def handle_docs_photo(message):
@@ -122,64 +148,78 @@ def botactions():
             file_info = bot.get_file(message.document.file_id)
             downloaded_file = bot.download_file(file_info.file_path)
 
-            src = 'way_to_dream.xlsx'
+            src = f'{BASE_DIR}/way_to_dream.xlsx'
             with open(src, 'wb') as new_file:
                 new_file.write(downloaded_file)
 
             bot.reply_to(message, "Рабочая таблица заменена")
         except Exception as e:
-            bot.reply_to(message, e)    
+            bot.reply_to(message, e)
 
     @bot.message_handler(content_types=['text'])
     def lalala(message):
         if message.chat.type == 'private':
             if message.text == 'Закончить работу':
-                #выбираем рандомную гифку
-                random = randint(1,3)
+                # выбираем рандомную гифку
+                random = randint(1, 3)
                 if random == 1:
-                    vid = open('bot/stickers/sponge_bob_thumb_up_gif_compressed.gif','rb')
+                    vid = open(
+                        f'{BASE_DIR}/bot/stickers/sponge_bob_thumb_up_gif_compressed.gif', 'rb')
                 elif random == 2:
                     vid = "CAACAgIAAxkBAAIh_2PzyPT4xVlQbF53WdPJw1eOuVXHAAIPAAMOR8coKchfPDiosqMuBA"
                 elif random == 3:
-                    vid = open('bot/stickers/win.gif','rb')
+                    vid = open(f'{BASE_DIR}/bot/stickers/win.gif', 'rb')
 
-                nums =  types.InlineKeyboardMarkup(row_width=8)
-                one =   types.InlineKeyboardButton(text='1', callback_data='one')
-                two =   types.InlineKeyboardButton(text='2', callback_data='two')
-                three = types.InlineKeyboardButton(text='3', callback_data='three')
-                four =  types.InlineKeyboardButton(text='4', callback_data='four')
-                five =  types.InlineKeyboardButton(text='5', callback_data='five')
-                nums.add(one,two,three,four,five)
-                
+                nums = types.InlineKeyboardMarkup(row_width=8)
+                one = types.InlineKeyboardButton(text='1', callback_data='one')
+                two = types.InlineKeyboardButton(text='2', callback_data='two')
+                three = types.InlineKeyboardButton(
+                    text='3', callback_data='three')
+                four = types.InlineKeyboardButton(
+                    text='4', callback_data='four')
+                five = types.InlineKeyboardButton(
+                    text='5', callback_data='five')
+                nums.add(one, two, three, four, five)
 
-                bot.send_animation(chat_id=message.chat.id, animation=vid,reply_to_message_id=message.id)
-                bot.send_message(chat_id=message.chat.id,text='<b>Ты легенда</b>\nЯ записал, время твоего начала\nВведи сколько заказов ты сделал',parse_mode='html',reply_markup=nums)
+                bot.send_animation(chat_id=message.chat.id,
+                                   animation=vid, reply_to_message_id=message.id)
+                bot.send_message(chat_id=message.chat.id, text='<b>Ты легенда</b>\nЯ записал, время твоего начала\nВведи сколько заказов ты сделал',
+                                 parse_mode='html', reply_markup=nums)
             else:
-                #отработка всех сообщений формат x руб и внесение значение в таблицу
+                # отработка всех сообщений формат x руб и внесение значение в таблицу
                 if 'руб' in message.text:
-                    rubs = message.text  
-                    rubs = int(rubs.replace(' руб',''))
-                    vklad = round((int(rubs) * 0.55),1)
-                    mamy = round((int(rubs) * 0.15),1)
+                    rubs = message.text
+                    rubs = int(rubs.replace(' руб', ''))
+                    vklad = round((int(rubs) * 0.55), 1)
+                    mamy = round((int(rubs) * 0.15), 1)
                     work_data.money(rubs)
                     work_data.stop()
-                    link_bank = types.InlineKeyboardMarkup()  
-                    my_vklad = types.InlineKeyboardButton(text='Перевести на вклад', url='ya.ru') 
-                    mamy_bank =   types.InlineKeyboardButton(text='Перевод маме', url='t.me')
-                    link_bank.add(my_vklad,mamy_bank)
-                    #формируем вывод
-                    enter = bot.send_message(message.chat.id, f"<b>Вы заработали</b>: <code>{rubs}</code>\n<b>Нужно отложить:</b> <code>{vklad}</code>\n<b>Перевести маме:</b> <code>{mamy}</code>",parse_mode='html',reply_markup=link_bank)
-                    
-                    excel = open('way_to_dream.xlsx','rb')
-                    bot.send_document(message.chat.id,excel,reply_to_message_id=enter.id)
+                    link_bank = types.InlineKeyboardMarkup()
+                    my_vklad = types.InlineKeyboardButton(
+                        text='Перевести на вклад', url='ya.ru')
+                    mamy_bank = types.InlineKeyboardButton(
+                        text='Перевод маме', url='t.me')
+                    link_bank.add(my_vklad, mamy_bank)
+                    # формируем вывод
+                    enter = bot.send_message(
+                        message.chat.id, f"<b>Вы заработали</b>: <code>{rubs}</code>\n<b>Нужно отложить:</b> <code>{vklad}</code>\n<b>Перевести маме:</b> <code>{mamy}</code>", parse_mode='html', reply_markup=link_bank)
 
+                    excel = open(f'{BASE_DIR}/way_to_dream.xlsx', 'rb')
+                    bot.send_document(message.chat.id, excel,
+                                      reply_to_message_id=enter.id)
+                elif 'n = ' in message.text:
+                    count = message.text
+                    count = int(count.replace('n = ', ''))
+                    data = {"count_run": count}
+                    with open(f'{BASE_DIR}/bot/count_run.json', 'w') as f:
+                        json.dump(data, f)
 
 
 polling_thread = threading.Thread(target=bot_polling)
 polling_thread.daemon = True
 polling_thread.start()
 
-#Keep main program running while bot runs threaded
+# Keep main program running while bot runs threaded
 if __name__ == "__main__":
     while True:
         try:
--- bot/work_data.py
+++ bot/work_data.py
@@ -1,68 +1,70 @@
 import json
-from pathlib import Path
 from datetime import date, datetime
+from pathlib import Path
 
 from openpyxl import load_workbook
 
-today = date.today
 BASE_DIR = Path(__file__).resolve().parent.parent
 wb = load_workbook(filename=f'{BASE_DIR}/way_to_dream.xlsx')
 ws = wb['новая сводка']
 
-data = json.load(open('bot\count_run.json'))
+data = json.load(open(f'{BASE_DIR}/bot/count_run.json'))
 n = data["count_run"]
-# прописываем кординаты с учетом счетчика
 
 def type(type) -> str:  # вводим на чем заработали
     global n
 
-    data = json.load(open('bot\count_run.json'))
+    data = json.load(open(f'{BASE_DIR}/bot/count_run.json'))
     n = data["count_run"]
-    
-    type_link   = f"G{2+n}"  # на чем заработал
+
+    type_link = f"G{2+n}"  # на чем заработал
 
     ws[type_link] = type  # сохраним в ячейку с датой сегодняшнюю
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
+
 
 def dates():  # сохраним дату сегодня
-    dates_link  = f"A{2+n}"  # ссылка на ячейку с датой
-
+    dates_link = f"A{2+n}"  # ссылка на ячейку с датой
+    today = date.today()
     ws[dates_link] = today  # сохраним в ячейку с датой сегодняшнюю
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
+
 
 def money(money) -> int:  # запишем сколько заработали
-    money_link  = f"B{2+n}"  # ссылка на сколько заработал
+    money_link = f"B{2+n}"  # ссылка на сколько заработал
 
     ws[money_link] = money  # сохраним в ячейку с датой сегодняшнюю
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
+
 
 def counts(count) -> int:  # просим ввести сколько заказов
     counts_link = f"C{2+n}"  # сколько заказов
 
     ws[counts_link] = count  # сохраним в ячейку с датой сегодняшнюю
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
+
 
 def start():  # записываем время сейчас старт
-    start_link  = f"D{3+n}"  # колхоз начало работы
+    start_link = f"D{3+n}"  # колхоз начало работы
 
     now = datetime.now()
     ws[start_link] = now.strftime("%H:%M")
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
 
 
 def stop():  # записываем время конца
     global n
-    
-    stop_link   = f"D{4+n}"  # колхоз конец  работы
-    time_link   = f"D{2+n}"  # разница времени работы
-    start_link  = f"D{3+n}"  # колхоз начало работы
+
+    stop_link = f"D{4+n}"  # колхоз конец  работы
+    time_link = f"D{2+n}"  # разница времени работы
+    start_link = f"D{3+n}"  # колхоз начало работы
 
     now = datetime.now()
     ws[stop_link] = now.strftime("%H:%M")
     ws[time_link] = f'={stop_link}-{start_link}'
-    wb.save('way_to_dream.xlsx')
+    wb.save(f'{BASE_DIR}/way_to_dream.xlsx')
 
     n += 1
     data = {"count_run": n}
-    with open("bot\count_run.json",'w') as f:
-        json.dump(data,f)
+    with open(f'{BASE_DIR}/bot/count_run.json', 'w') as f:
+        json.dump(data, f)
